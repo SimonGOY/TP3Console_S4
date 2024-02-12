b8e739fa62179eaac025d2d5781505b8a33e946b
SIMON GOY <sg.bonneville@gmail.com>
2024-02-12 10:56:26 +0100

    Ajoutez des fichiers projet.

---
+++ TP3Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TP3Console.Models.EntityFramework;
+
+namespace TP3Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new FilmsDBContext())
+            {
+                //Chargement de la catégorie Action, des films de cette catégorie et des avis
+                Categorie categorieAction = ctx.Categories
+                .Include(c => c.Films)
+                .ThenInclude(f => f.Avis)
+                .First(c => c.Nom == "Action");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
