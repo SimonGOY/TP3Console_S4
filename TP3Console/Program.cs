2f62245f0a0b0cfe93b4dae286ad0e17ff16fa05
SIMON GOY <sg.bonneville@gmail.com>
2024-02-14 11:33:55 +0100

    Fin TP3

--- TP3Console/Program.cs
+++ TP3Console/Program.cs
@@ -2,6 +2,7 @@
 using TP3Console.Models.EntityFramework;
 using System.Linq;
 using System.Globalization;
+using System.Diagnostics;
 
 namespace TP3Console
 {
@@ -31,7 +32,13 @@ namespace TP3Console
                 .First(c => c.Nom == "Action");
             }*/
 
-            Exo2Q9();
+            //Exo2Q9();
+
+            //AjoutUtilisateur();
+            //ModifFilm();
+            //SupprimeFilm();
+            //AjouterAvis();
+            AjoutFilms();
 
             Console.ReadKey();
         }
@@ -102,6 +109,8 @@ namespace TP3Console
         public static void Exo2Q7()
         {
             var ctx = new FilmsDBContext();
+            /*CultureInfo ci = new CultureInfo("fr-FR")
+            var films = ctx.Films.Where(s => s.Nom.StartsWith("LE", true, ci));*/ // Auttre manière de le faire
             var films = ctx.Films.Where(s => s.Nom.ToUpper().StartsWith("LE"));
             foreach(var film in films)
             {
@@ -121,6 +130,93 @@ namespace TP3Console
         {
             var ctx = new FilmsDBContext();
             Console.WriteLine(ctx.Utilisateurs.First(s => s.Id == ctx.Avis.First(s=>s.Note == ctx.Avis.Max(s=>s.Note)).Utilisateur));
+        }
+
+        public static void AjoutUtilisateur()
+        {
+            using (var ctx = new FilmsDBContext() )
+            {
+                var user = new Utilisateur()
+                {
+                    Login = "Simon",
+                    Email = "simon.goy@etu.univ-smb.fr",
+                    Pwd = "nop"
+                };
+                ctx.Add(user);
+
+                ctx.SaveChanges();
+            }
+        }
+
+        public static void ModifFilm()
+        {
+            using (var ctx = new FilmsDBContext())
+            {
+                var film = ctx.Films.First<Film>(s=>s.Nom.ToLower() == "l'armee des douze singes");
+                var cate = ctx.Categories.First(ctx=>ctx.Nom == "Drame").Id;
+                film.Categorie = cate;
+                ctx.SaveChanges();
+            }
+        }
+
+        public static void SupprimeFilm()
+        {
+            using (var ctx = new FilmsDBContext())
+            {
+                var film = ctx.Films.First<Film>(s => s.Nom.ToLower() == "l'armee des douze singes");
+                var id = film.Id;
+                var avis = ctx.Avis.Where(s=>s.Film == id); 
+                ctx.Films.Remove(film);
+                foreach(var aviss in avis)
+                    ctx.Avis.Remove(aviss);
+                ctx.SaveChanges();
+            }
+        }
+
+        public static void AjouterAvis()
+        {
+            using (var ctx = new FilmsDBContext())
+            {
+                var avis = new Avi()
+                {
+
+                    Film = 3,
+                    Utilisateur = 1,
+                    Avis = "Il est bien",
+                    Note = 0.8M
+                };
+                ctx.Add(avis);
+
+                ctx.SaveChanges();
+            }
+        }
+        public static void AjoutFilms()
+        {
+            using (var ctx = new FilmsDBContext())
+            {
+                Categorie cate = ctx.Categories.First(ctx => ctx.Nom == "Drame");
+                List<Film> liste = new List<Film>();
+                int id = ctx.Films.Max(s => s.Id);
+                var film = new Film()
+                {
+                    Id = id +1,
+                    Nom = "Le film",
+                    Description = "bah un film",
+                    CategorieNavigation = cate
+                };
+                var film2 = new Film()
+                {
+                    Id = id + 2,
+                    Nom = "Le film2",
+                    Description = "nah un film la suite",
+                    CategorieNavigation = cate
+                };
+                liste.Add(film);
+                liste.Add(film2);
+                ctx.AddRange(liste);
+
+                ctx.SaveChanges();
+            }
         }
     }
 }
